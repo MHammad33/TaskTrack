393af7dbccbbe566237cb293a257ada09a61b0b0
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-01-20 21:53:28 +0500

    Save tasks to Local Storage option added
    Now users can save tasks to local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Task from "./components/Task/Task";
 import TaskForm from "./components/TaskForm/TaskForm";
 
 function App() {
 	const [tasks, setTasks] = useState([]);
+
+	useEffect(() => {
+		if (tasks.length === 0) return;
+		localStorage.setItem("tasks", JSON.stringify(tasks));
+	}, [tasks]);
+
+	useEffect(() => {
+		const lsTasks = JSON.parse(localStorage.getItem("tasks"));
+		setTasks(lsTasks);
+	}, []);
 
 	const addTask = (name) => {
 		setTasks((prev) => {
